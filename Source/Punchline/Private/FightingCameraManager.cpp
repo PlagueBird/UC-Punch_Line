ea0dcf0e9dbec2e7318350d3d6396f330f05ac3f
Zachary Brown <brownz4@mail.uc.edu>
2025-03-03 17:59:18 -0500

    finally working ish camera

---
+++ Source/Punchline/Private/FightingCameraManager.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FightingCameraManager.h"
+#include "GameFramework/Actor.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AFightingCameraManager::AFightingCameraManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFightingCameraManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFightingCameraManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+void AFightingCameraManager::UpdateCamera(float DeltaTime)
+{
+    Super::UpdateCamera(DeltaTime);
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), BP_PlayerCharacter, PlayerCharacters);    
+    // Get player characters
+   /* AActor* Player1 = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+    AActor* Player2 = UGameplayStatics::GetPlayerCharacter(GetWorld(), 1);
+  
+    if (Player1 && Player2) 
+    {
+        FVector Player1Location = Player1->GetActorLocation();
+        FVector Player2Location = Player2->GetActorLocation();
+
+        FVector NewCameraLocation = CalculateCameraPosition(Player1Location, Player2Location);
+        SetActorLocation(NewCameraLocation);
+    }*/
+}
+
+FVector AFightingCameraManager::CalculateCameraPosition(const FVector& Player1Location, const FVector& Player2Location)
+{
+    FVector MidPoint = (Player1Location + Player2Location) / 2.0f;
+    float Distance = FVector::Dist(Player1Location, Player2Location);
+
+    float CameraDistance = FMath::Clamp(Distance, MinCameraDistance, MaxCameraDistance);
+    FVector CameraLocation = MidPoint + FVector(0.0f, 0.0f, CameraHeight) - FVector(CameraDistance, 0.0f, 0.0f);
+
+    return CameraLocation;
+}
+
---
+++ Source/Punchline/Punchline.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Punchline.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Punchline, "Punchline" );
